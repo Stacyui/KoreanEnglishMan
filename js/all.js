11277969b6d0ab58b2dc934dd3daf85cfe4a6496
yui.fujikawa <yui.fujikawa@micro-wave.net>
2023-02-15 15:24:23 +0900

    first commit

---
+++ js/all.js
@@ -0,0 +1,13 @@
+$(function() {
+  console.log('document ready complete');
+
+  $(".openbtn1").click(function () {
+    $(this).toggleClass('active');
+      $("#g-nav").toggleClass('panelactive');
+  });
+  
+  $("#g-nav a").click(function () {
+      $(".openbtn1").removeClass('active');
+      $("#g-nav").removeClass('panelactive');
+  });
+});
